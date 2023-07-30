5b407bf13518b700d1b212b66720b4ac10bada14
John Holt <jdholt77@earthlink.net>
2023-07-30 17:12:57 -0400

    Initial Commit

---
+++ XplorePDF/XplorePDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  XplorePDFApp.swift
+//  XplorePDF
+//
+//  Created by John Holt on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct XplorePDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
